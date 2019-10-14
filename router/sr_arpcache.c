e74f9f59f104ba6e6246857b384865542b385cf1
Qikai Guo <qikai.guo@mail.toronto.ca>
2019-10-14 16:44:25 -0400

    a

--- router/sr_arpcache.c
+++ router/sr_arpcache.c
@@ -35,6 +35,7 @@ void handle_arpreq(struct sr_instance *sr, struct sr_arpreq *req) {
             struct sr_packet *packet = req->packets;
             while (packet) {
                 sr_ip_hdr_t* p_ihdr = (sr_ip_hdr_t*)(packet + sizeof(sr_ethernet_hdr_t));
+                print_hdr_ip(p_ihdr);
                 send_icmp_packet(sr, p_ihdr, packet->iface, 3, 1, p_ihdr->ip_dst, p_ihdr->ip_src);
                 packet = packet->next;
             }
